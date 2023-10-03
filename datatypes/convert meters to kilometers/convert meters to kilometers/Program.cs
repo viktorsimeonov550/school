fdeffbe2c8f39d331276ddfdaae03190266f35f8
viktorsimeonov550 <viktor.simeonov@yahoo.com>
2023-10-03 13:42:30 +0300

    added task one

---
+++ datatypes/convert meters to kilometers/convert meters to kilometers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert_meters_to_kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+            decimal n = num / 1000;
+
+            Console.WriteLine(value: $"{n:f2}");
+        }
+    }
+}
